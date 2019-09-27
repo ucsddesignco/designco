035db79341c303108845bbffbec95c2dc38904a3
Emily Nguyen <emilymylinh@gmail.com>
2019-09-27 15:21:53 -0700

    Add sponsors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,11 @@ import { spacer1, spacer2, spacer3, spacer4 } from "../constants";
 import placeholder from "../images/placeholder/fb.png";
 import { setConfiguration } from "react-grid-system";
 
+import intuit from "../images/sponsors/intuit.png";
+import sony from "../images/sponsors/sony.png";
+import fb from "../images/sponsors/fb.png";
+import dlab from "../images/sponsors/dlab.png";
+
 class Home extends React.Component {
   render() {
     return (
@@ -35,15 +40,37 @@ class Home extends React.Component {
                   6:30PM
                 </p>
               </Col>
-              <Col sm={6}>
-              </Col>
+              <Col sm={6}></Col>
             </Row>
-            <Row>
+            <Row style={{ marginBottom: spacer4 }}>
               <Col>
                 <Button
                   link="https://www.facebook.com/pg/ucsddesignco/events/"
                   text="View All Events"
                 ></Button>
+              </Col>
+            </Row>
+            <h2 style={{ marginBottom: spacer3 }}>Sponsors</h2>
+            <Row>
+              <Col xs={6} md={3} style={{ marginBottom: spacer2 }}>
+                <a href="https://www.intuit.com/" target="_blank">
+                  <img src={intuit} alt="Intuit"></img>
+                </a>
+              </Col>
+              <Col xs={6} md={3} style={{ marginBottom: spacer2 }}>
+                <a href="https://www.sony.com/" target="_blank">
+                  <img src={sony} alt="Sony"></img>
+                </a>
+              </Col>
+              <Col xs={6} md={3} style={{ marginBottom: spacer2 }}>
+                <a href="https://facebook.com" target="_blank">
+                  <img src={fb} alt="Facebook"></img>
+                </a>
+              </Col>
+              <Col xs={6} md={3} style={{ marginBottom: spacer2 }}>
+                <a href="https://designlab.ucsd.edu/" target="_blank">
+                  <img src={dlab} alt="The Design Lab at UC San Diego"></img>
+                </a>
               </Col>
             </Row>
           </div>
