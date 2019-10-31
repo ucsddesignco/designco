cad1e0278684082fc9ab18e9c8df1484604eaa7f
andrewzhen <ajzhen@ucsd.edu>
2019-10-30 20:03:19 -0700

    Deploy Sanity

--- ucsddesignco/schemas/schema.js
+++ ucsddesignco/schemas/schema.js
@@ -1,18 +1,8 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
+
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-// We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import product from './product'
-import vendor from './vendor'
-import productVariant from './productVariant'
-
-import localeString from './locale/String'
-import localeText from './locale/Text'
-import localeBlockContent from './locale/BlockContent'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -21,17 +11,18 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    product,
-    vendor,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    localeText,
-    localeBlockContent,
-    localeString,
-    productVariant
+    /* Your types here! */
+    {
+    	name: 'event',
+    	type: 'document',
+    	title: 'Event',
+    	fields: [
+    		{
+    			name: 'title',
+    			type: "string",
+    			title: "Title",
+    		}
+    	]
+    }
   ])
 })
