56fad4832d6957129c370d0e535d84ee26965059
andrewzhen <ajzhen@ucsd.edu>
2020-04-27 14:54:00 -0700

    change replace to href

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -3,7 +3,7 @@ import React from 'react'
 class GBM extends React.Component {
   render() {
     return (
-      window.location.replace("https://ucsd.zoom.us/j/93237758725")
+      window.location.href= "https://ucsd.zoom.us/j/93237758725"
     )
   }
 }
