861eb6f067c1101b58f0871ccb525c2cf2de7d4f
andrewzhen <ajzhen@ucsd.edu>
2020-04-27 21:54:27 -0700

    undo redirect

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Home from '../pages/Home'
 
 class GBM extends React.Component {
-  componentDidMount() {
-    window.location.href= "https://ucsd.zoom.us/j/93237758725"
-  }
+  // componentDidMount() {
+  //   window.location.href= "https://ucsd.zoom.us/j/93237758725"
+  // }
 
   render() {
     return (
