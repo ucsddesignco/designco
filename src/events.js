6ee540bdb6d1c26ada8b5f2e9f59600c10118bc5
andrewzhen <ajzhen@ucsd.edu>
2020-05-18 16:04:15 -0700

    add events

--- src/events.js
+++ src/events.js
@@ -2,7 +2,7 @@ export const events = [
   {
     title: "GBM 1: Design Co Fall Kickoff",
     link: "https://www.facebook.com/events/492692744911435/",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/s960x960/71183159_2373152389591885_2782785340589473792_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=F-u93j01rTkAX_xvcXI&_nc_ht=scontent-lax3-1.xx&_nc_tp=7&oh=f5a43287602b9d52f116e85987dd2ad6&oe=5EC2F5B7",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/71183159_2373152389591885_2782785340589473792_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=qQ53ixIfL9UAX8ALXGw&_nc_ht=scontent-lax3-1.xx&oh=e504bbe25d731b123070d44ee5461a90&oe=5EE803D2",
     date: 20191003,
     time: 1830,
     location: "HSS 1346"
@@ -51,7 +51,7 @@ export const events = [
   {
     title: "Product Design Talk w/ Facebook",
     link: "https://www.facebook.com/events/2557519091031116/",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/72755128_2395395577367566_739480337716871168_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=xBGwKdP7xGQAX8rMjEu&_nc_ht=scontent-lax3-1.xx&oh=506d5ae3fbebcaa9deb66b2c72374d87&oe=5EC2AF1F",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/72755128_2395395577367566_739480337716871168_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=AWSJG6UVyMoAX--5vMw&_nc_ht=scontent-lax3-1.xx&oh=c54b06307b3a8f1788271a0775248821&oe=5EEA3C1F",
     date: 20191029,
     time: 1830,
     location: "HSS 1346"
@@ -67,7 +67,7 @@ export const events = [
   {
     title: "Studio Session: Visual Design Principles w/ Facebook",
     link: "https://www.facebook.com/events/441147996760078/",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/s960x960/75435872_2393368987570225_7052494792915156992_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=2ld98opLIfMAX9PNdcF&_nc_ht=scontent-lax3-1.xx&_nc_tp=7&oh=3ab6e6caf7a9e80d5d8c4629b3517a46&oe=5EC0D958",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/75435872_2393368987570225_7052494792915156992_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=3k0M_-szh9UAX-iBa_o&_nc_ht=scontent-lax3-1.xx&oh=0c69bd7296168207c2b813f1397cba35&oe=5EE6F4B1",
     date: 20191030,
     time: 1830,
     location: "HSS 1346"
@@ -99,7 +99,7 @@ export const events = [
   {
     title: "Design Co: Portfolio Day",
     link: "https://www.facebook.com/events/267052910893182/",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/74643527_2409883179252139_490790852126310400_o.jpg?_nc_cat=100&_nc_ohc=quIWlAKk0OgAX-92Hb8&_nc_ht=scontent-lax3-1.xx&oh=ccc2012b7bffaffca42a098d4e15fd54&oe=5EC100E1",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/s960x960/74643527_2409883179252139_490790852126310400_o.jpg?_nc_cat=100&_nc_sid=b386c4&_nc_ohc=78Y-L22bQK0AX-sQsn3&_nc_ht=scontent-lax3-1.xx&_nc_tp=7&oh=b2cfc9bff6ffcd31bc671413d9118308&oe=5EE99DDE",
     date: 20191117,
     time: 1200,
     location: "The Forum, Price Center 4th Floor"
@@ -206,8 +206,7 @@ export const events = [
   {
     title: "Design Co: Brand Design Workshop",
     link: "https://www.facebook.com/events/1474078526092634/",
-    image:
-      "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/86217666_2496080080632448_3565264588771426304_o.jpg?_nc_cat=109&_nc_ohc=5nPPgsG3WjwAX9t9Fd5&_nc_ht=scontent-lax3-1.xx&oh=2ba7c5da60b0dcb0e8b45fc4a14732b9&oe=5EC1A090",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/86217666_2496080080632448_3565264588771426304_o.jpg?_nc_cat=109&_nc_sid=b386c4&_nc_ohc=OYrZcLB0YnsAX8tpGby&_nc_ht=scontent-lax3-1.xx&oh=e8189d7d365a36e5566b5bebf8ecd756&oe=5EE6E970",
     date: 20200219,
     time: 1830,
     location: "HSS 1346"
@@ -215,7 +214,7 @@ export const events = [
   {
     title: "Design Co: Front-End Development Workshop",
     link: "https://www.facebook.com/events/3569320876442160/",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-0/p480x480/87162340_2505437833030006_2103682244370497536_o.jpg?_nc_cat=105&_nc_ohc=2P0JsOY_tRAAX9M51rc&_nc_ht=scontent-lax3-1.xx&_nc_tp=6&oh=0b0f3591e05b20e7de0c1e680c246f36&oe=5EC1B146",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/87162340_2505437833030006_2103682244370497536_o.jpg?_nc_cat=105&_nc_sid=b386c4&_nc_ohc=0h-aa-p7rVQAX-hkIhX&_nc_ht=scontent-lax3-1.xx&oh=875b8161b22d2fa7684399692872342f&oe=5EE8F2A3",
     date: 20200226,
     time: 1830,
     location: "HSS 1346"
@@ -239,7 +238,7 @@ export const events = [
   {
     title: "Design Co: End of Quarter Celebration",
     link: "https://www.facebook.com/events/1045820139124085",
-    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-0/p480x480/89782901_2517422061831583_109240762301939712_o.jpg?_nc_cat=100&_nc_sid=b386c4&_nc_ohc=N3wXETGD8CcAX88DXuX&_nc_ht=scontent-lax3-1.xx&_nc_tp=6&oh=c822ced33be576b7f1b1139daa3afd2b&oe=5EC1FFA7",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/89782901_2517422061831583_109240762301939712_o.jpg?_nc_cat=100&_nc_sid=b386c4&_nc_ohc=0aZmOAuOGB0AX-QELAs&_nc_ht=scontent-lax3-1.xx&oh=4ac18926031a97d56dc0923d03201530&oe=5EE6C39E",
     date: 20200311,
     time: 1830,
     location: "SpotKTV: 7327 Clairemont Mesa Blvd"
@@ -298,6 +297,22 @@ export const events = [
     image: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/96390590_2565359273704528_1639323394077884416_o.jpg?_nc_cat=107&_nc_sid=b386c4&_nc_ohc=ywbozC0HPiAAX-w1cWA&_nc_ht=scontent-lax3-2.xx&oh=2d30d0b50a569e3ced71565c3731f5eb&oe=5EE776FA",
     date: 20200513,
     time: 1700,
+    location: "Zoom"
+  },
+  {
+    title: "Design Co: Design Inspiration",
+    link: "https://www.facebook.com/events/712529716219165/",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/99099802_2573036982936757_5163440856123834368_o.jpg?_nc_cat=104&_nc_sid=b386c4&_nc_ohc=js1inSIXiK0AX-vKHRj&_nc_ht=scontent-lax3-1.xx&oh=e102168efb6c70310170abb1324193d9&oe=5EE8E3C9",
+    date: 20200520,
+    time: 1700,
+    location: "Zoom"
+  },
+  {
+    title: "Design Co: Netflix Party Social",
+    link: "https://www.facebook.com/events/167606648027766/",
+    image: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-0/p480x480/97748101_2570741789832943_3988981550816428032_o.jpg?_nc_cat=109&_nc_sid=b386c4&_nc_ohc=Ug9F-WOoucoAX83zRn1&_nc_ht=scontent-lax3-1.xx&_nc_tp=6&oh=fa7240e4bfa15f628cbf31cf6db32537&oe=5EE9BB1C",
+    date: 20200522,
+    time: 2000,
     location: "Zoom"
   }
 ];
