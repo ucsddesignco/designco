2a5a6f10446368ca45d22fc635c7cf10fb636c70
Adam Syed <syedadamemirputra@gmail.com>
2020-10-28 00:13:07 +0800

    Pushed!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -86,6 +86,25 @@ class Home extends React.Component {
                 </p>
                 <p className="text_caption">Sept. 30, 2020</p>
               </Col>
+              <Col
+                sm={6}
+                className="events innerContainer"
+                style={{ marginBottom: spacer3 }}
+              >
+                <h2 style={{ marginBottom: spacer3 }}>Upcoming Events</h2>
+                <div className="upcomingEventsContainer">
+                  <Row style={{ marginBottom: spacer2 }}>{eventList}</Row>
+                  <Row style={{ marginBottom: spacer4 }}>
+                    <Col>
+                      <Button
+                        style={{ marginBottom: spacer3 }}
+                        link="/events"
+                        text="View All Events"
+                      ></Button>
+                    </Col>
+                  </Row>
+                </div>
+              </Col>
             </Row>
           </div>
           <div
