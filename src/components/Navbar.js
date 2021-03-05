d65c30ce8e34192b09a00fb7236519298b43940d
Adam Syed <syedadamemirputra@gmail.com>
2021-03-05 13:25:48 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -272,13 +272,13 @@ class Navbar extends React.Component {
                             </a>
                           </h4>
                         </li>
-                        {/* <li>
+                        <li>
                           <h4>
                             <a href="/join-our-team" id="join-our-team">
                               Join Our Team
                             </a>
                           </h4>
-                        </li> */}
+                        </li>
                         {/* <li>
                           <h4>
                             <a
