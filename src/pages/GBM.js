9c4e58a73ce27e3eaa28ed52c4e92075ba35dc51
Adam Syed <syedadamemirputra@gmail.com>
2021-04-13 22:05:59 -0700

    Update GBM.js

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -2,8 +2,11 @@ import React from "react";
 
 class GeneralBodyMeeting extends React.Component {
   render() {
-    return (window.location = "https://ucsd.zoom.us/j/93237758725");
+    return (window.location =
+      "https://urldefense.com/v3/__https://teams.microsoft.com/l/meetup-join/19*3ameeting_ZTE1ZTkxZjktNWI3MS00ZDQ0LTgxNjYtZDQyNWRhMzdmYTJm*40thread.v2/0?context=*7b*22Tid*22*3a*22e0793d39-0939-496d-b129-198edd916feb*22*2c*22Oid*22*3a*22d04eb7a6-b367-4c23-9d73-38ec6f5be956*22*7d__;JSUlJSUlJSUlJSUlJSUl!!Mih3wA!RniiW-T8c45YDjimBDzNtTRHK9dfs4BdpPtMiny4b7HvNVvkPo8nsVLIq5JYJg$");
   }
 }
 
 export default GeneralBodyMeeting;
+
+// GBM: https://ucsd.zoom.us/j/93237758725
