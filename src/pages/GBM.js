da4a0fdf7f03a77ccd6484c2df171a8d44f236d3
Adam Syed <syedadamemirputra@gmail.com>
2021-04-13 22:07:26 -0700

    Update GBM.js

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -3,7 +3,7 @@ import React from "react";
 class GeneralBodyMeeting extends React.Component {
   render() {
     return (window.location =
-      "https://urldefense.com/v3/__https://teams.microsoft.com/l/meetup-join/19*3ameeting_ZTE1ZTkxZjktNWI3MS00ZDQ0LTgxNjYtZDQyNWRhMzdmYTJm*40thread.v2/0?context=*7b*22Tid*22*3a*22e0793d39-0939-496d-b129-198edd916feb*22*2c*22Oid*22*3a*22d04eb7a6-b367-4c23-9d73-38ec6f5be956*22*7d__;JSUlJSUlJSUlJSUlJSUl!!Mih3wA!RniiW-T8c45YDjimBDzNtTRHK9dfs4BdpPtMiny4b7HvNVvkPo8nsVLIq5JYJg$");
+      "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTE1ZTkxZjktNWI3MS00ZDQ0LTgxNjYtZDQyNWRhMzdmYTJm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e0793d39-0939-496d-b129-198edd916feb%2522%252c%2522Oid%2522%253a%2522d04eb7a6-b367-4c23-9d73-38ec6f5be956%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=a62868b3-3677-40cf-8292-6705244fbeb8&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true");
   }
 }
 
