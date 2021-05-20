ad64ec6662eb36af73db894c91052bc58881f40c
Adam Syed <syedadamemirputra@gmail.com>
2021-05-19 18:14:37 -0700

    Update GBM.js

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -2,7 +2,8 @@ import React from "react";
 
 class GeneralBodyMeeting extends React.Component {
   render() {
-    return (window.location = "https://tinyurl.com/smalldesignworld");
+    return (window.location =
+      "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTE1ZTkxZjktNWI3MS00ZDQ0LTgxNjYtZDQyNWRhMzdmYTJm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522e0793d39-0939-496d-b129-198edd916feb%2522%252c%2522Oid%2522%253a%2522d04eb7a6-b367-4c23-9d73-38ec6f5be956%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=d8d56c3b-fbee-4908-84d8-8741ee6e849b&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true");
   }
 }
 
