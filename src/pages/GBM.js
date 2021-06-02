f67f2741c219e6064368b2ebfad1e6de2c5ed5bf
Adam Syed <syedadamemirputra@gmail.com>
2021-06-01 22:12:37 -0700

    GBM Link

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -3,7 +3,7 @@ import React from "react";
 class GeneralBodyMeeting extends React.Component {
   render() {
     return (window.location =
-      "https://us02web.zoom.us/j/82853695045?fbclid=IwAR2ATBsD5aDkZHIHRaCAaRZkzcxyXpjs5jPKgtS8bTCZRwO2GpBZrU2S89Y");
+      "https://us02web.zoom.us/j/82853695045?fbclid=IwAR2gY7_JqJasy7niGqXGrmcfD_ay9wWLmhHN_zuW9mk3ku1-r6x52RAfFHE");
   }
 }
 
