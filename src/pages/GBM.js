d53ce49029658d515ae053fc941932b8a4101138
Adam Syed <syedadamemirputra@gmail.com>
2021-06-28 17:02:09 -0700

    Power Up Link

--- src/pages/GBM.js
+++ src/pages/GBM.js
@@ -2,8 +2,7 @@ import React from "react";
 
 class GeneralBodyMeeting extends React.Component {
   render() {
-    return (window.location =
-      "https://us02web.zoom.us/j/82853695045?fbclid=IwAR2gY7_JqJasy7niGqXGrmcfD_ay9wWLmhHN_zuW9mk3ku1-r6x52RAfFHE");
+    return (window.location = "https://ucsd.zoom.us/j/98679923476");
   }
 }
 
