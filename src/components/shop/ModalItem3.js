abe5771c93f483d40e9b0997f2b6af40221fc3df
Shawn Kim <sjk002@ucsd.edu>
2021-10-22 21:04:43 -0700

    Details

--- src/components/shop/ModalItem3.js
+++ src/components/shop/ModalItem3.js
@@ -38,7 +38,7 @@ function ModalItem3(props) {
                                 <p id="m1-5b" className="smaller-text">Every designer has their own story. It is time to create yours.</p>
                                 <p id="m1-5b" className="smaller-text">Designed by Design Co Creative Team</p>
                                 <p className="smaller-text">Models in Order of Photos:</p>
-                                <p id="m1-5b" className="smaller-text">Juna: 5'3" - M, Julie: 5'5" - L, Jamin: 5'10" - L, Adam: 6'0" - M</p>
+                                <p id="m1-5b" className="smaller-text">Juna: 5'3" - M, Julie: 5'5" - L, Adam: 6'0" - M, Maria: 5'7" - M, Jamin: 5'10" - L</p>
                             
                                 {/* <p id="m1-5b" className="smaller-text">Juna, (1st) is 5'3" and is wearing size Medium. Julie (second picture) is 5'5" and is wearing size Large. Jamin (third picture) is 5'10" and is wearing size Large. Adam (fourth picture) is 6'0" and is wearing size Medium.</p> */}
                             </Grid>
