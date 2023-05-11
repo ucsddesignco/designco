ce6ea0daaa68dac4a98f75b1f52d2c5521d1ae45
gambowoa <baymarket96@gmail.com>
2023-05-10 18:21:01 -0700

    adjust modal

--- src/components/shop/ShopPhotoGallery/ShopPhotoGallery.js
+++ src/components/shop/ShopPhotoGallery/ShopPhotoGallery.js
@@ -5,21 +5,23 @@ import { useState } from "react";
 const ShopPhotoGallery = ({ images = [] }) => {
   const [photoIndex, setPhotoIndex] = useState(0);
   return (
-    <div>
-      <div className="shop-photo-gallery_main-image-wrapper">
-        <img src={images[photoIndex]} alt="" />
-        <button
-          className="shop-photo-gallery_control left"
-          onClick={() => {
-            setPhotoIndex(Math.max(0, photoIndex - 1));
-          }}
-        />
-        <button
-          className="shop-photo-gallery_control right"
-          onClick={() => {
-            setPhotoIndex(Math.min(images.length - 1, photoIndex + 1));
-          }}
-        />
+    <div className="shop-photo-gallery">
+      <div className="shop-photo-gallery_preview">
+        <div className="shop-photo-gallery_main-image-wrapper">
+          <img src={images[photoIndex]} alt="" />
+          <button
+            className="shop-photo-gallery_control left"
+            onClick={() => {
+              setPhotoIndex(Math.max(0, photoIndex - 1));
+            }}
+          />
+          <button
+            className="shop-photo-gallery_control right"
+            onClick={() => {
+              setPhotoIndex(Math.min(images.length - 1, photoIndex + 1));
+            }}
+          />
+        </div>
       </div>
       <div className="shop-photo-gallery_thumbnail-image-row">
         {images.map((i, index) => (
