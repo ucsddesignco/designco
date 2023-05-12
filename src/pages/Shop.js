6d8b4483b7d096ba9cf2a481d3eb9646bb893933
brianliu1412 <briangliu1@gmail.com>
2023-05-12 13:55:05 -0700

    Final Draft of Shop Page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import WhiteFooter from "../components/WhiteFooter";
 import ShopOverlay from "../images/video/shop-overlay.mp4";
+import herovid from "../images/video/hero.mp4";
 
 //import shop items list
 import carrot from "../images/shop/downchevron.svg";
@@ -9,61 +10,87 @@ import AutoTiles from "../components/AutoTiles/AutoTiles";
 import AutoTile from "../components/AutoTiles/subcomponents/AutoTile";
 import { ShopItem } from "../components/shop/ShopItem/ShopItem";
 import { ShopModal } from "../components/shop/ShopModal/ShopModal";
+
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+// Image Imports
+const crewneck = importAll(require.context('../images/newshop/crewneck/', false, /\.(png|jpe?g|svg)$/));
+const shirt = importAll(require.context('../images/newshop/shirt/', false, /\.(png|jpe?g|svg)$/));
+const hero = importAll(require.context('../images/newshop/hero/', false, /\.(png|jpe?g|svg)$/));
+const stickers = importAll(require.context('../images/newshop/sticker/', false, /\.(png|jpe?g|svg)$/));
+
+
 const shopItems = [
   {
     title: "PLAY3 Embroidered Crewneck",
-    price: "$35,000",
+    price: "$35",
     images: [
-      "https://placehold.co/600x400/orange/white?text=Crewneck",
-      "https://placehold.co/600x400/blue/white?text=Crewneck",
-      "https://placehold.co/600x400/green/white?text=Crewneck",
+      crewneck[0].default,
+      crewneck[1].default,
+      crewneck[2].default,
+      crewneck[3].default,
+      crewneck[4].default,
+      crewneck[5].default,
+      crewneck[6].default,
+      crewneck[7].default,
+      crewneck[8].default,
+
     ],
-    overlayImage: "https://placehold.co/600x400/red/white?text=Crewneck%20Zoom",
+    overlayImage: crewneck[1].default,
     description: (
       <>
         <p>
-          Don’t be afraid to play around! It’s time to play, play, play to your
-          heart’s content. Grab your pencils and crayons — on your mark, get
-          set, design!
+        Don’t be afraid to play around! It’s time to play, play, play to your heart’s content. 
+        Grab your pencils and crayons — on your mark, get set, design!
         </p>
         <p>80/20 cotton/polyester blend; Alpine green</p>
         <p>Embroidered front design, screen-printed back design</p>
-        <p>Designed by Juna Kim</p>
         <p>
-          Models in order of appearance: Khoi is 5’6” wearing size M, Brooke is
-          _’_” wearing size M.
+          Models in order of appearance: Khoi is 5’6” wearing size M, 
+          Brooke is 5’0” wearing size M, Nel is 5’5” wearing size S.
         </p>
+        <p>Designed by Juna Kim</p>
       </>
     ),
     sizes: [
       { size: "S", available: true },
-      { size: "M", available: false },
-      { size: "L", available: false },
+      { size: "M", available: true },
+      { size: "L", available: true },
     ],
   },
   {
     title: "It’s OK to Make Mistakes Shirt",
-    price: "$18,000",
+    price: "$18",
     images: [
-      "https://placehold.co/600x400/blue/white?text=Shirt",
-      "https://placehold.co/600x400/magenta/white?text=Shirt",
+      shirt[0].default,
+      shirt[1].default,
+      shirt[2].default,
+      shirt[3].default,
+      shirt[4].default,
+      shirt[5].default,
+      shirt[6].default,
+      shirt[7].default,
+      shirt[8].default,
+
     ],
-    overlayImage: "https://placehold.co/600x400/orange/white?text=Shirt%20Zoom",
+    overlayImage: shirt[1].default,
 
     description: (
       <>
         <p>
-          Don’t be afraid to play around! It’s time to play, play, play to your
-          heart’s content. Grab your pencils and crayons — on your mark, get
-          set, design!
+          Design doesn’t have to be stressful with the It’s OK to Make Mistakes shirt; 
+          embrace your childlike imagination & have a silly little time.
         </p>
-        <p>80/20 cotton/polyester blend; Alpine green</p>
-        <p>Embroidered front design, screen-printed back design</p>
+        <p>100% ring spun cotton; Bay green</p>
+        <p>Screen-printed front and back design</p>
         <p>Designed by Juna Kim</p>
         <p>
-          Models in order of appearance: Khoi is 5’6” wearing size M, Brooke is
-          _’_” wearing size M.
+          Models in order of appearance: Nel is 5’5” wearing size S, Brooke is 5’0” wearing size L, 
+          Khoi is 5’6” wearing size M, Sahil is 6’1” wearing size M.
         </p>
+        <p>Designed by Megan Tan</p>
       </>
     ),
     sizes: [
@@ -73,36 +100,37 @@ const shopItems = [
   },
   {
     images: [
-      "https://placehold.co/600x400/green/white?text=Logo%20Sticker",
-      "https://placehold.co/600x400/blue/white?text=Logo%20Sticker",
+      stickers[1].default,
+      stickers[0].default,
     ],
-    overlayImage:
-      "https://placehold.co/600x400/orange/white?text=Logo%20Sticker%20Zoom",
+    overlayImage: stickers[0].default,
     title: "Design Co Black Holographic Logo Sticker",
-    price: "$2,000",
+    price: "$2",
     description: (
       <>
-        <p>Very cool logo sticker.</p>
-        <p>80/20 cotton/polyester blend; Alpine green</p>
+        <p>Rep Design Co with your very own DCo logo sticker – this time, in dark mode.</p>
+        <p>2" x 2” holographic logo on glossy black background</p>
       </>
     ),
     sizes: [],
   },
   {
-    images: ["https://placehold.co/600x400/red/white?text=Playtime%20Sticker"],
-    overlayImage:
-      "https://placehold.co/600x400/orange/white?text=Playtime%20Sticker%20Zoom",
+    images: [stickers[2].default, stickers[3].default,],
+    overlayImage: stickers[3].default,
     title: "It’s Playtime! Holographic Sticker",
-    price: "$3,000",
+    price: "$3",
     description: (
       <>
-        <p>Very cool playtime sticker.</p>
-        <p>80/20 cotton/polyester blend; Alpine green</p>
+        <p>There’s no time to clean up your desk when your brain is working on overdrive 
+          sketching all your design ideas. Make a mess, and yoink this comic-style sticker!</p>
+        <p>4” x 3” holographic</p>
+        <p>Designed by Juna Kim</p>
       </>
     ),
     sizes: [],
   },
 ];
+
 
 function Shop() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -121,7 +149,7 @@ function Shop() {
     <div className="shop">
       <div className="shop-background">
         <video autoPlay loop muted>
-          <source src={ShopOverlay} type="video/mp4" />
+          <source src={herovid} type="video/mp4" />
         </video>
       </div>
       <Navbar color={"#FFF"} altColor={"#000"} from="shop" />
@@ -151,7 +179,7 @@ function Shop() {
         </div>
       </div>
       <div className="shop-gallery" id="gallery-section">
-        <h2 className="shop-gallery-title">Browse Our Collection</h2>
+        <h2 className="shop-gallery-title">Playtime Club for Designers Collection</h2>
         <div className="shop-gallery-items">
           <AutoTiles minWidth={250} space={40}>
             {shopItems.map((i, index) => (
